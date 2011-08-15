2e2b16b75c396fc2e0f126b0500bc901dc8d1446
imzzk <zhangkun@cnblogs.com>
2011-08-15 14:36:35 +0800

    加入AppRecord实体类。

---
+++ LazyTableView/LazyTableView/AppRecord.m
@@ -0,0 +1,23 @@
+//
+//  AppRecord.m
+//  LazyTableView
+//
+//  Created by 坤坤 on 11-8-15.
+//  Copyright 2011年 博客园. All rights reserved.
+//
+
+#import "AppRecord.h"
+
+@implementation AppRecord
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
