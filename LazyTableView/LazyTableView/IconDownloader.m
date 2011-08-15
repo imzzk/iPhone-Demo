456ba8d15374bd7a2da7ee03c5ac0dbd751a0b4f
imzzk <zhangkun@cnblogs.com>
2011-08-15 16:17:55 +0800

    加入icondownloader

---
+++ LazyTableView/LazyTableView/IconDownloader.m
@@ -0,0 +1,23 @@
+//
+//  IconDownloader.m
+//  LazyTableView
+//
+//  Created by 坤坤 on 11-8-15.
+//  Copyright 2011年 博客园. All rights reserved.
+//
+
+#import "IconDownloader.h"
+
+@implementation IconDownloader
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
