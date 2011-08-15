fdd8e0f990135bddaa4554cd4cdbde8fbb448e98
imzzk <zhangkun@cnblogs.com>
2011-08-16 07:40:21 +0800

    xml 解析

---
+++ LazyTableView/LazyTableView/ParseOperation.h
@@ -0,0 +1,13 @@
+//
+//  ParseOperation.h
+//  LazyTableView
+//
+//  Created by 坤坤 on 11-8-15.
+//  Copyright 2011年 博客园. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseOperation : NSObject
+
+@end
---
+++ LazyTableView/LazyTableView/ParseOperation.m
@@ -0,0 +1,23 @@
+//
+//  ParseOperation.m
+//  LazyTableView
+//
+//  Created by 坤坤 on 11-8-15.
+//  Copyright 2011年 博客园. All rights reserved.
+//
+
+#import "ParseOperation.h"
+
+@implementation ParseOperation
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
